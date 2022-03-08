ac1a13c9a7bf36a586a5302b8c5f6ca24b5e450f
healygi <healygi@tcd.ie>
2022-03-08 17:31:58 +0000

    Add basic structure of game grid in js. Add array and images of each card, create function to loop through cards. Add css styling to display game grid.

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -3,28 +3,75 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const cardArray = [
 {
-    name: 'alien',
-    img: ''
+    name: 'asteroid',
+    img: 'assets/css/images/asteroid.jpg'
 },
 {
-    name: 'alien',
-    img:''
+    name: 'asteroid',
+    img: 'assets/css/images/asteroid.jpg'
 },
 {
-    name: 'alien',
-    img:''
+    name: 'earth',
+    img: 'assets/css/images/earth.jpg'
 },
 {
-    name: 'alien',
-    img:''
+    name: 'earth',
+    img: 'assets/css/images/earth.jpg'
 },
 {
-    name: 'alien',
-    img:''
+    name: 'galaxy',
+    img:'assets/css/images/galaxy.jpg'
 },
 {
-    name: 'alien',
-    img:''
+    name: 'galaxy',
+    img:'assets/css/images/galaxy.jpg'
 },
+{
+    name: 'moon',
+    img:'assets/css/images/moon.jpg'
+},
+{
+    name: 'moon',
+    img:'assets/css/images/moon.jpg'
+},
+{
+    name: 'nasa',
+    img:'assets/css/images/nasa.jpg'
+},
+{
+    name: 'nasa',
+    img:'assets/css/images/nasa.jpg'
+},
+{
+    name: 'planet',
+    img:'assets/css/images/planet.jpg'
+},
+{
+    name: 'planet',
+    img:'assets/css/images/planet.jpg'
+}
 ]
+
+//defining grid in js
+const grid = document.querySelector('.grid')
+
+
+//create your board and loop over array
+function createBoard() {
+    for(let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'assets/css/images/card.jpg')
+        card.setAttribute('data-id', i)
+        grid.appendChild(card)
+    }
+}
+
+createBoard()
+
+//check for matches
+
+
+
+//flip your card
+
 })
