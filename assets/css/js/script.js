e15f5e66a8c5041b7b172d271de5b5dd498c86d7
healygi <healygi@tcd.ie>
2022-03-08 13:34:58 +0000

    Add basic js structure for card game array.

---
+++ assets/css/js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+//card options
+
+const cardArray = [
+{
+    name: 'alien',
+    img: ''
+},
+{
+    name: 'alien',
+    img:''
+},
+{
+    name: 'alien',
+    img:''
+},
+{
+    name: 'alien',
+    img:''
+},
+{
+    name: 'alien',
+    img:''
+},
+{
+    name: 'alien',
+    img:''
+},
+]
+})
